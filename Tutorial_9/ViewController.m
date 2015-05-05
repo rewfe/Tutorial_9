60eb25df49c8515c0fd2448b3aa56c027ba0f21b
rewfe <qrewfe@yandex.ru>
2015-05-05 04:46:46 +0300

    Use Storyboards to Build NavController

--- Tutorial_9/ViewController.m
+++ Tutorial_9/ViewController.m
@@ -13,9 +13,13 @@
 @end
 
 @implementation ViewController
-
+{
+NSArray *recipes;
+}
 - (void)viewDidLoad {
     [super viewDidLoad];
+    recipes = [NSArray arrayWithObjects:@"Egg Benedict", @"Mushroom Risotto", @"Full Breakfast", @"Hamburger", @"Ham and Egg Sandwich", @"Creme Brelee", @"White Chocolate Donut", @"Starbucks Coffee", @"Vegetable Curry", @"Instant Noodle with Egg", @"Noodle with BBQ Pork", @"Japanese Noodle with Pork", @"Green Tea", @"Thai Shrimp Cake", @"Angry Birds Cake", @"Ham and Cheese Panini", nil];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -24,4 +28,22 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [recipes count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"RecipeCell";
+    
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
+    }
+    
+    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
+    return cell;
+}
 @end
